bc8f4d78b8f001f591220bdc62f9c3e8d462780e
Angus211-beep <nikora.t.denton@gmail.com>
2025-06-29 00:45:09 +1200

    Update AngusHub.lua

--- AngusHub.lua
+++ AngusHub.lua
@@ -39,3 +39,15 @@ local Window = Rayfield:CreateWindow({
 
 local PlayerTab = Window:CreateTab("Player", nil) -- Title, Image
 local Section = PlayerTab:CreateSection("Movement")
+
+local Slider = PlayerTab:CreateSlider({
+   Name = "WalkSpeed",
+   Range = {0, 300},
+   Increment = 5,
+   Suffix = "Bananas",
+   CurrentValue = 16,
+   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Value)
+   game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+   end,
+})
